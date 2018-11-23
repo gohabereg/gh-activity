326f40691318119bc8bbdb123deb95b116df3274
georgyb <yegor.berezhnoi@3d4medical.com>
2018-11-23 20:47:00 +0300

    Setup authentication strategy to work with chrome extension

--- src/authentication.js
+++ src/authentication.js
@@ -1,5 +1,6 @@
 const authentication = require('@feathersjs/authentication');
 const jwt = require('@feathersjs/authentication-jwt');
+const url = require('url');
 
 const oauth2 = require('@feathersjs/authentication-oauth2');
 const GithubStrategy = require('passport-github');
@@ -7,13 +8,29 @@ const GithubStrategy = require('passport-github');
 module.exports = function (app) {
   const config = app.get('authentication');
 
+  const handler = async (req, res, next) => {
+    if (req.feathers && req.feathers.payload) {
+      try {
+        const token = await app.passport.createJWT(req.feathers.payload, {
+          jwt: config.jwt,
+          secret: config.secret
+        });
+
+        res.redirect(url.resolve(config.github.successRedirect, `./${token}`));
+      } catch (e) {
+        next(e);
+      }
+    }
+  }
+
   // Set up authentication with the secret
   app.configure(authentication(config));
   app.configure(jwt());
 
   app.configure(oauth2(Object.assign({
     name: 'github',
-    Strategy: GithubStrategy
+    Strategy: GithubStrategy,
+    handler
   }, config.github)));
 
   // The `authentication` service is used to create a JWT.
